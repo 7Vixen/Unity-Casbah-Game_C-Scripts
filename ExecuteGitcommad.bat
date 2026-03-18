19e28dcede21fbd6365fab32ad2dc961b8c115d4
Vixen-77 <redjahyousra6@gmail.com>
2026-03-18 01:16:50 +0100

    add bat command commit

---
+++ ExecuteGitcommad.bat
@@ -0,0 +1,11 @@
+
+// ne xecuté pas cette partie ta3i hadia 
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/7Vixen/Unity-Casbah-Game_C-Scripts.git
+git push -u origin main
+
+// vous aver qua push ou pull 
+// de préference cree votre porpre branch
